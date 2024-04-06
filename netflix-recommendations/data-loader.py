c7a50ae8baa67e1e24966c6bf4877f2deeaf74d7
Kahaan Gandhi <kahaan@gmail.com>
2024-04-06 18:48:04 -0400

    clean up and functionize preprocessing

--- netflix-recommendations/data-loader.py
+++ netflix-recommendations/data-loader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# Read in subset of full Netflix prize data as DataFrame
 def load_subset(data_path, check=False):
     train_path = data_path + "training_ratings.txt"
     test_path = data_path + "testing_ratings.txt"
@@ -41,3 +42,67 @@ def load_subset(data_path, check=False):
         print(f"The smallest amount of entries for any UserID is: {smallest_entries_count}")
 
     return df_subset
+
+# Convert large .txt file into structured DataFrame
+def transform_file_to_dataframe(file_path):
+    transformed_data = []
+    with open(file_path, 'r') as file:
+        current_movie_id = None  
+        for line in file:
+            line = line.strip()
+            if line.endswith(':'):  
+                # If current line is movie ID, strip colon to capture ID
+                current_movie_id = line[:-1]  
+            else:
+                # Otherwise, split by commas and prepend most recent ID
+                user_id, rating, date = line.split(',')
+                transformed_data.append([current_movie_id, user_id, rating, date])
+                
+    columns = ['MovieID', 'UserID', 'Rating', 'Date']
+    dataframe = pd.DataFrame(transformed_data, columns=columns)
+    return dataframe
+
+# Read full Netflix prize data as DataFrame 
+def load_netflix_data(data_path, check=False):
+    title_path = data_path + "movie_titles.txt"
+    genre_path = data_path + "movie_genres.csv"
+    
+    # Read in movie titles line by line
+    titles = []
+    with open(title_path, 'r', encoding='latin1') as file:
+        for line in file:
+            # Maximum of 2 splits per line, avoiding commas in movie name
+            parts = line.strip().split(',', 2)
+            # Verify succesful split
+            if len(parts) == 3:
+                titles.append(parts)
+            else:
+                print(f"Skipped malformed line: {line}")
+    
+    df_titles = pd.DataFrame(titles, columns=['MovieID', 'Year', 'Title'])
+    df_genres = pd.read_csv(genre_path, names=["MovieID", "Genres"]).drop(index=0)
+    df_genres['MovieID'] = df_genres['MovieID'].astype(int)
+    df_titles['MovieID'] = df_titles['MovieID'].astype(int)
+    
+    # Full datasets available at https://www.kaggle.com/datasets/netflix-inc/netflix-prize-data/data
+    df_1 = transform_file_to_dataframe(data_path + "combined_data_1.txt")
+    df_2 = transform_file_to_dataframe(data_path + "combined_data_2.txt")
+    df_3 = transform_file_to_dataframe(data_path + "combined_data_3.txt")
+    df_4 = transform_file_to_dataframe(data_path + "combined_data_4.txt")
+    
+    # Merge datasets
+    df_12 = pd.concat([df_1, df_2])
+    df_34 = pd.concat([df_3, df_4])
+    df_1234 = pd.concat([df_12, df_34])
+    df_1234["MovieID"] = df_1234["MovieID"].astype(int)
+    df_1234_genres = pd.merge(df_1234, df_genres, on="MovieID", how="left")
+    df_titles["MovieID"] = df_titles["MovieID"].astype(int)
+    df_final = pd.merge(df_1234_genres, df_titles, on="MovieID", how="left")
+
+    if check:
+        # Check entries per user
+        entries_per_user = df_final.groupby('UserID').size()
+        smallest_entries_count = entries_per_user.min()
+        print(f"The smallest amount of entries for any UserID is: {smallest_entries_count}")
+
+    return df_final
